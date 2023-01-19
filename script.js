2d8c723155b8b5f89f7811748ff4d6610f52468b
Nabil <darknab@live.fr>
2023-01-19 09:57:52 +0100

    Created the grid and a hover effect

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let gridSize = 16;
 const container = document.querySelector(".container")
-const square = document.createElement("div");
-square.classList.add(".square")
-container.appendChild(square);
+//const square = document.createElement("div");
+//square.classList.add = "square";
+const squares = [];
+squares.length = gridSize*gridSize;
+container.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr);`
 
-
-/*for (let i = 0; i < (gridSize*gridSize); i++) {
-    container.appendChild(square);
-    square.classList.add = ".square";
-
-}*/
+for (let i = 0; i < squares.length; i++) {
+    squares[i] = document.createElement("div");
+    squares[i].classList.add("square")
+    container.appendChild(squares[i]);
+}
