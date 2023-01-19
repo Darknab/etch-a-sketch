7c34da2ed9f84f96affed5075ca129fc1e03a8b7
Nabil <darknab@live.fr>
2023-01-19 08:05:51 +0100

    Created the html, css and js files and added container and a button

---
+++ script.js
@@ -0,0 +1,12 @@
+let gridSize = 16;
+const container = document.querySelector(".container")
+const square = document.createElement("div");
+square.classList.add(".square")
+container.appendChild(square);
+
+
+/*for (let i = 0; i < (gridSize*gridSize); i++) {
+    container.appendChild(square);
+    square.classList.add = ".square";
+
+}*/
